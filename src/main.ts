a44882c23dadabd97b7ec67467c2b613a6e1ef40
Karol Przygoda <karolprzygodastudia@gmail.com>
2026-01-08 00:25:36 +0100

    Update manifest to restrict plugin to desktop only, remove unused CSS animations, and clean up modal class references in TypeScript files.

--- src/main.ts
+++ src/main.ts
@@ -139,7 +139,6 @@ class FileSelectionModal extends Modal {
     super(plugin.app);
     this.plugin = plugin;
     this.action = action;
-    this.modalEl.addClass('tag-modal');
   }
 
   onOpen() {
@@ -346,7 +345,6 @@ abstract class BaseTagModal extends Modal {
       depthLevel: -1,
     };
     this.existingTags = [];
-    this.modalEl.addClass('tag-modal');
   }
 
   async onOpen() {
